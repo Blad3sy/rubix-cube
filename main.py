aab9d529244fa97e11d0bf5749bdc88617e29987
Bladesy <oscarprosser@gmail.com>
2022-11-10 17:08:37 +0000

    Changed Face() and made the actual generation of a Rubik's Cube
    - Setting squares of Face() is now simplified
    
    - scramble() exists, which, when given 6 faces, generates 9 instances of each colour (currently represented as a number) for each of the 54 squares visible
    
    NOTE : yes I know rubik's is misspelt as 'rubix'
    
    NOTE : The current code generating a random rubik's cube will, most of the time, generate a cube that is impossible to solve. This is something that i've decided to just deal with, and maybe when i'm a better programmer I can come back to this and add a better method ( maybe generating a solved cube and randomly turning it or something ), but I'm not that programmer yet.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint, choice
+
 class Face():
 
     def __init__(self, left, right, above, below):
@@ -6,9 +8,21 @@ class Face():
         self.right = right
         self.above = above
         self.below = below
+        self.squaresH = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        self.squaresV = [[None, None, None], [None, None, None], [None, None, None]]
 
-        self.squaresV = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
-        self.squaresH = [[self.squaresV[0][0], self.squaresV[1][0], self.squaresV[2][0]], [self.squaresV[0][1], self.squaresV[1][1], self.squaresV[2][1]], [self.squaresV[0][2], self.squaresV[1][2], self.squaresV[2][2]]]
+        self.setSquares(True)
+    
+    def setSquares(self, vertical):
+
+        if vertical:
+            for i in range(0, 3):
+                for t in range(0, 3):
+                    self.squaresV[i][t] = self.squaresH[t][i] 
+        else:
+            for i in range(0, 3):
+                for t in range(0, 3):
+                    self.squaresH[i][t] = self.squaresV[t][i]          
 
     def push(self, direction, row, rowNum, vertical):
 
@@ -18,7 +32,49 @@ class Face():
 
         if vertical:
             self.squaresV[rowNum] = row
-            self.squaresH = [[self.squaresV[0][0], self.squaresV[1][0], self.squaresV[2][0]], [self.squaresV[0][1], self.squaresV[1][1], self.squaresV[2][1]], [self.squaresV[0][2], self.squaresV[1][2], self.squaresV[2][2]]]
+            self.setSquares(False)
         else:
             self.squaresH[rowNum] = row
-            self.squaresV = [[self.squaresV[0][0], self.squaresV[1][0], self.squaresV[2][0]], [self.squaresV[0][1], self.squaresV[1][1], self.squaresV[2][1]], [self.squaresV[0][2], self.squaresV[1][2], self.squaresV[2][2]]]
+            self.setSquares(True)
+
+def scramble(face1, face2, face3, face4, face5, face6):
+    # generates 9 coordinates for each colour and allocates them
+    # if coord is already occupied (not 0, which is white), generate new one
+    # order is faceNum, rowNum, cellNum
+
+    faceList = [face1, face2, face3, face4, face5, face6]
+
+    for i in range(1, 6):
+        for t in range(0, 9):
+            allocatedFalse = True
+            while allocatedFalse:
+                faceSelect = choice(faceList)
+                rowNum = randint(0, 2)
+                cellNum = randint(0, 2)
+
+                if faceSelect.squaresH[rowNum][cellNum] == 0:
+                    faceSelect.squaresH[rowNum][cellNum] = i
+                    allocatedFalse = False
+
+face1 = Face(None, None, None, None)
+face2 = Face(None, None, None, None)
+face3 = Face(None, None, None, None)
+face4 = Face(None, None, None, None)
+face5 = Face(None, None, None, None)
+face6 = Face(None, None, None, None)
+
+face1.__init__(face2, face3, face4, face5)
+face2.__init__(face6, face1, face4, face5)
+face3.__init__(face1, face6, face4, face5)
+face4.__init__(face2, face3, face6, face1)
+face5.__init__(face2, face3, face1, face6)
+face6.__init__(face2, face3, face5, face4)
+
+scramble(face1, face2, face3, face4, face5, face6)
+
+print(face1.squaresH)
+print(face2.squaresH)
+print(face3.squaresH)
+print(face4.squaresH)
+print(face5.squaresH)
+print(face6.squaresH)
